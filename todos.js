0300795a4d988c6b9a7ad888c17cdd5126c6204a
JunLargo <rklargsjr@gmail.com>
2019-02-21 23:03:40 +0800

    created new object for todoList

--- todos.js
+++ todos.js
@@ -1,1 +1,26 @@
-alert('hello');
+var todoList = {
+    todos: ['one', 'two', 'three'],
+    displayTodos: function() {
+        console.log(this.todos);
+    },
+    addTodo: function(newTodo) {
+        this.todos.push(newTodo);
+        this.displayTodos();
+    },
+    changeTodo: function(todoPosition, todoValue) {
+        this.todos[todoPosition] = todoValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(remove) {
+        this.todos.splice(remove, 1);
+        this.displayTodos();
+    }
+};
+
+todoList.deleteTodo(2);
+
+
+
+
+
+
