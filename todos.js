19498007b17620048756c3ca02f14e879e53f847
prillargo30 <aprilkilariolargo30@gmail.com>
2019-02-24 00:20:11 +0800

    finished Boolean

--- todos.js
+++ todos.js
@@ -1,21 +1,21 @@
-var todoList = {
-    todos: ['one', 'two', 'three'],
-    displayTodos: function() {
-        console.log(this.todos);
-    },
-    addTodo: function(newTodo) {
-        this.todos.push(newTodo);
-        this.displayTodos();
-    },
-    changeTodo: function(todoPosition, todoValue) {
-        this.todos[todoPosition] = todoValue;
-        this.displayTodos();
-    },
-    deleteTodo: function(remove) {
-        this.todos.splice(remove, 1);
-        this.displayTodos();
-    }
-};
+// var todoList = {
+//     todos: ['one', 'two', 'three'],
+//     displayTodos: function() {
+//         console.log(this.todos);
+//     },
+//     addTodo: function(newTodo) {
+//         this.todos.push(newTodo);
+//         this.displayTodos();
+//     },
+//     changeTodo: function(todoPosition, todoValue) {
+//         this.todos[todoPosition] = todoValue;
+//         this.displayTodos();
+//     },
+//     deleteTodo: function(remove) {
+//         this.todos.splice(remove, 1);
+//         this.displayTodos();
+//     }
+// };
 
 
 
@@ -25,25 +25,31 @@ var todoList = {
 //Pls work on version 4 - Booleans, I'll proceed with Version - 5 Loops of Logic
 
 var todoList = {
-    todos: []
-    displayTodos: function () {
-        console.log('My todos:'todos);
-}
-},
-addTodo: function (todoText) {
-        this.todos.push({
-            todoText: todoText,
-            completed: false
-        });
+    todos: [],
+    displayTodos: function (){
+        console.log ('My todos:', this.todos);
+    },
+    addTodos: function (todoText) {
+    this.todos.push({
+        todoText: todoText,
+        completed: false
+    });
+    this.displayTodos();
+    },
+    changeTodos: function (position, todoText){
+        this.todos[position].todoText = todoText;
         this.displayTodos();
+    }, 
+    toggleCompleted: function (position){
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
 }
-// changeTodo: function (position, todoText) {
-//     this.todos [position].todoText = todoText;
-//     this.displayTodos();
-// }
 
 // todoList.addTodos("hi");
 //jun command me to type whatever title
+
 
 
 
